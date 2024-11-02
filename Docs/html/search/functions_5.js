c31152782ca97434b2f1300114209f407d4ca11e
mrtaichi <isaaniet@gmail.com>
2024-11-01 21:05:58 -0600

    Docs refresh

--- Docs/html/search/functions_5.js
+++ Docs/html/search/functions_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['setup_49',['setup',['../RoboCiencias_8ino.html#a4fc01d736fe50cf5b977f755b675f11d',1,'RoboCiencias.ino']]],
-  ['stance_50',['stance',['../classLegs.html#ac34784c0ab78823b672baf3e2daa6c32',1,'Legs']]]
+  ['stance_50',['stance',['../classLegs.html#ac34784c0ab78823b672baf3e2daa6c32',1,'Legs::stance(Stances stance)'],['../classLegs.html#a379500d804200ba0ff2c5e01cfc7f266',1,'Legs::stance()']]]
 ];
